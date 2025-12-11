383c5491a18ab123bbf380e5d0b61d8d43ab2b7e
Akshay Chauhan <akshay.399488@gmail.com>
2025-12-11 17:48:07 +0530

    new 2

---
+++ src/test/java/stepDefinitions/Stepnew2.java
@@ -0,0 +1,18 @@
+package stepDefinitions;
+
+import java.security.PublicKey;
+import java.util.jar.Attributes.Name;
+
+public class Stepnew2 {
+
+	
+	public static  void name() {
+		System.out.println("hi");
+	}
+	 
+	 public static void main(String[] args) {
+		
+		 Stepnew2 stepnew = new Stepnew2();
+		 stepnew.name();
+	}
+}
